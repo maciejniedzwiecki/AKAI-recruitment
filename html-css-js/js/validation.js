4d10e6bebe2f742244c60ae4b8f073c89eb47370
Maciej <maciejniedzwiecki.01@gmail.com>
2022-10-14 15:08:07 +0200

    solution

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -1,11 +1,43 @@
-/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    AKAI Frontend Task - Javascript
+function validationForm() {
+	let form = document.getElementById('form')
+	let firstName = document.getElementById('first-name').value
+	let lastName = document.getElementById('last-name').value
+	let email = document.getElementById('email').value
 
-    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
-    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
-    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
+	form.addEventListener('submit', e => {
+		e.preventDefault()
 
-    Powodzenia!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
+		validationForm()
+	})
+
+	let f = document.getElementById('frontend-checkbox')
+	let b = document.getElementById('backend-checkbox')
+	let m = document.getElementById('mobile-checkbox')
+	let g = document.getElementById('graphics-checkbox')
+
+	form.addEventListener('submit', function validateCheckbox(e) {
+		e.preventDefault()
+		if (f.checked === true || b.checked === true || m.checked === true || g.checked === true) {
+			return true
+		} else {
+			alert('Nie wybrałeś żadnej sekcji! Spróbuj ponownie.')
+			return false
+		}
+	})
+
+	if (firstName == '') {
+		alert('Imię nie może pozostać puste! Spróbuj ponownie.')
+		document.myForm.firstName.focus()
+		return false
+	}
+	if (lastName == '') {
+		alert('Nazwisko nie może pozostać puste! Spróbuj ponownie.')
+		return false
+	}
+	if (email == '') {
+		alert('Pole adresu e-mail nie może pozostać puste! Spróbuj ponownie.')
+		return false
+	} else {
+		return true
+	}
+}
